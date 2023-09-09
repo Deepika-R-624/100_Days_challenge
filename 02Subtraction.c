b43588505facabac00db401e3f0060e5d3354455
Deepika R <143886322+Deepika-R-624@users.noreply.github.com>
2023-09-09 22:15:48 +0530

    Day3

---
+++ 02Subtraction.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int x,y;
+    printf("enter the number");
+    scanf("%d",&x);
+    y=x-5;
+    printf("%d",y);
+    return 0;
+}
