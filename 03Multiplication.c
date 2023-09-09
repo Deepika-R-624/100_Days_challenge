a4f50a3a5f14e0d436f914a40520a394579defb9
Deepika R <143886322+Deepika-R-624@users.noreply.github.com>
2023-09-09 22:16:31 +0530

    Day3

---
+++ 03Multiplication.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int a,b;
+    printf("enter the number");
+    scanf("%d",&a);
+    b=a*3;
+    printf("%d",b);
+    return 0;
+}
