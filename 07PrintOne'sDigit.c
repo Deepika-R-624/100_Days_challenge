6afc3a604d3c6d9d503d76d331bf9e48846f8c04
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2023-09-15 23:55:28 +0530

    Day7

---
+++ 07PrintOne'sDigit.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+      int x,y;
+printf("enter three digit number");
+scanf("%d",&x);
+y=(x%10);
+printf("%d",y);
+    return 0;
+}
